4617173787fe9e357d26bb7131173b25f670e116
Average-Bear <jeremyndewitt@gmail.com>
2017-11-02 00:02:56 +1200

    Update Disable-InactiveUsers.ps1

--- Disable-InactiveUsers.ps1
+++ Disable-InactiveUsers.ps1
@@ -18,9 +18,11 @@
 param(
 
     [Parameter(ValueFromPipeline=$true,HelpMessage='Enter desired OU to search for inactive users (90 days); (i.e. "OU=Users, DC=FOO, DC=BAR, DC=COM")')]
+    [ValidateNotNullOrEmpty()] 
     [String[]]$SearchOU = "OU=01_Users, DC=ACME, DC=COM",
     
     [Parameter(ValueFromPipeline=$true,HelpMessage='Enter desired destination OU for stale accounts (120 days); (i.e. "OU=DeleteTheseAccounts, OU=Users, DC=FOO, DC=BAR, DC=COM")')]
+    [ValidateNotNullOrEmpty()] 
     [String]$AccountsToDeleteOU = 'OU=Accounts_to_Delete, OU=Users, DC=ACME, DC=COM'
 )
 
